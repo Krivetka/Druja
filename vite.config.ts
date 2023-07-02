15462b1ca1eeec6980798242162ce1bc1a82757b
Asus <dfyreybr@gmail.com>
2023-07-02 21:24:39 +0200

    gh-page vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publickPath:'/Druja/',
   plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
